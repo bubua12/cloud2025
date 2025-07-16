3b70f7f14b92ecab74d604941465a6841d31fa7b
bubua12 <wangty334752@gmail.com>
2025-07-17 00:07:57 +0800

    feat: add order nacos client

---
+++ ms-services/service-order/src/main/java/com/bubua12/cloud/order/OrderMainApplication.java
@@ -0,0 +1,17 @@
+package com.bubua12.cloud.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author bubua12
+ * @since 2025/7/16 23:52
+ */
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+}
