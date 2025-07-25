26c36f1fe6f9742c390a41f0950b3727ab470654
bubua12 <wangty334752@gmail.com>
2025-07-25 23:23:12 +0800

    feat: add nacos config demo

--- ms-services/service-order/src/main/java/com/bubua12/cloud/order/controller/OrderController.java
+++ ms-services/service-order/src/main/java/com/bubua12/cloud/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.bubua12.cloud.order.controller;
 import com.bubua12.cloud.model.order.OrderVO;
 import com.bubua12.cloud.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bubua12
  * @since 2025/7/19 9:35
  */
+@RefreshScope // 激活配置属性的自动刷新功能
 @RestController
 public class OrderController {
 
     @Resource
     private OrderService orderService;
 
+    @Value("${order.timeout}")
+    private String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    private String orderAutoConfirm;
+
     @GetMapping("/create")
     public OrderVO createOrder(@RequestParam("userId") Long userId,
                                @RequestParam("productId") Long productId) {
         return orderService.createOrder(productId, userId);
+    }
+
+    @GetMapping("/config")
+    public String config() {
+        return "order.timeout: " + orderTimeout + "\torder.auto-confirm: " + orderAutoConfirm;
     }
 }
