5bf4c7c4dd2dc3afe3b3380d0028aa373edcf162
bubua12 <wangty334752@gmail.com>
2025-08-16 18:28:47 +0800

    feat: filter basic usage

--- ms-services/service-order/src/main/java/com/bubua12/cloud/order/controller/OrderController.java
+++ ms-services/service-order/src/main/java/com/bubua12/cloud/order/controller/OrderController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bubua12
  * @since 2025/7/19 9:35
  */
-@RequestMapping("/api/order")
+//@RequestMapping("/api/order")
 @RefreshScope // 激活配置属性的自动刷新功能
 @RestController
 @Slf4j
