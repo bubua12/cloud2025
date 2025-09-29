289f5d898edeebccda3e3f1d5150c88b0a42a2bc
wangxl <wangty334752@gmail.com>
2025-09-29 12:02:26 +0800

    feat: use micro tracing

--- ms-services/seata-order/src/main/java/com/bubua12/cloud/order/config/ObservationConfig.java
+++ ms-services/seata-order/src/main/java/com/bubua12/cloud/order/config/ObservationConfig.java
@@ -1,13 +1,14 @@
 package com.bubua12.cloud.order.config;
 
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 观测配置类
- * 配置链路追踪相关的Bean
+ * 配置链路追踪相关的Bean，包括数据库操作追踪
  *
  * @author bubua12
  * @since 2025/9/24
@@ -23,5 +24,14 @@ public class ObservationConfig {
     @Bean
     public ObservationRegistry observationRegistry() {
         return ObservationRegistry.create();
+    }
+    
+    /**
+     * 启用@Observed注解支持
+     * 用于Service层方法的链路追踪
+     */
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
     }
 }
--- ms-services/seata-storage/src/main/java/com/bubua12/cloud/storage/config/ObservationConfig.java
+++ ms-services/seata-storage/src/main/java/com/bubua12/cloud/storage/config/ObservationConfig.java
@@ -1,12 +1,13 @@
 package com.bubua12.cloud.storage.config;
 
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 观测配置类
- * 配置链路追踪相关的Bean
+ * 配置链路追踪相关的Bean，包括数据库操作追踪
  *
  * @author bubua12
  * @since 2025/9/24
@@ -21,5 +22,14 @@ public class ObservationConfig {
     @Bean
     public ObservationRegistry observationRegistry() {
         return ObservationRegistry.create();
+    }
+    
+    /**
+     * 启用@Observed注解支持
+     * 用于Service层方法的链路追踪
+     */
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
     }
 }
