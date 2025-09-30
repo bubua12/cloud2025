2aa27dba652e1836890ee353beb96f760092cce9
wangxl <wangty334752@gmail.com>
2025-09-30 11:14:27 +0800

    feat: use otel starter(with unique trace_id between ms services)

--- ms-services/seata-order/src/main/java/com/bubua12/cloud/order/SeataOrderMainApplication.java
+++ ms-services/seata-order/src/main/java/com/bubua12/cloud/order/SeataOrderMainApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableFeignClients
 @MapperScan("com.bubua12.cloud.order.mapper")
 public class SeataOrderMainApplication {
     public static void main(String[] args) {
